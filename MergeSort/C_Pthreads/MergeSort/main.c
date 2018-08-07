50d12da8af0102f526408b204527701104b96063
Andrew Le <andrewle19@csu.fullerton.edu>
2018-08-07 01:57:07 -0700

    finished read me

--- MergeSort/C_Pthreads/MergeSort/main.c
+++ MergeSort/C_Pthreads/MergeSort/main.c
@@ -24,16 +24,16 @@ typedef struct index{
 
 
 void *merge_sort(void * param) {
-    
-    
+
+
     index *pr = (index*) param;
-    
+
     int p = pr->p, r = pr->r;
-    
+
     if(p == r){
         pthread_exit(0);
     }
-    
+
     // two sides of the thread sort
     int sort1,sort2;
     // declare two threads
@@ -47,31 +47,31 @@ void *merge_sort(void * param) {
     side1.r = mid;
     side2.p = mid+1;
     side2.r = pr->r;
-    
+
     // create thread to sort half of the array
     sort1 = pthread_create(&thd1, NULL, merge_sort,(void*)&side1);
     if(sort1 >0){
         printf("Failed to create new thread1\n");
     }
-    
+
     // create thread to sort other half
     sort2 = pthread_create(&thd2, NULL, merge_sort,(void*)&side2);
     if(sort2 >0){
         printf("Failed to create new thread2\n");
     }
-    
+
     // join the threads
     pthread_join(thd1, NULL);
     pthread_join(thd2, NULL);
-    
-    
+
+
     int k = pr->p;
     int i = pr->p;
     int j = mid+1;
-    
+
     // MERGE SORT ALGORITHM
     while(i <= mid && j <= r){
-        
+
         if(a[i] < a[j]){
             b[k] = a[i];
             k++;
@@ -81,55 +81,55 @@ void *merge_sort(void * param) {
             k++;
             j++;
         }
-        
+
     }
     for (; i<=mid ; i++) {
          b[k] = a[i];
          k++;
     }
-    
+
     for (; j<=r ; j++){
         b[k] = a[j];
         k++;
     }
-    
+
     for (i= pr->p ; i <= r ;i++) {
         a[i] = b[i];
     }
-    
-    
+
+
     pthread_exit(0);
     return NULL;
 }
 
 int main() {
-    
+
     // make a parent thread
     pthread_t start_thread;
     b = (int*)malloc(sizeof(int)*SIZE);
-    
+
     // start index
     index start;
     start.p = 0;
     start.r = SIZE-1;
-    
+
     // print before the sort
     printf("Before Sort: ");
     for(int i = 0; i < SIZE; i++){
         printf("%i ",a[i]);
     }
     printf("\n");
-    
+
     // create thread to merge sort
     pthread_create(&start_thread, NULL, merge_sort, &start);
     pthread_join(start_thread,NULL);
-    
+
     // print after the sort
     printf("After Sort: ");
     for(int i = 0; i < SIZE; i++){
         printf("%i ",a[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 <h2>Author:Andrew Le<h2>
 <h2>Email:andrewle19@gmail.com<h2>
-
+<p> Projects are written in multiple languages which are listed below. Each sub project will use a different threading library.</p>
 <ol>
   <li>
-    <p><b>MergeSort</b></p>
+    <p><b>MergeSort:</b> uses MultiThreading to sort halves of arrays to perform the MergeSort Algorithm O(nlog(n))</p>
     <ul>
-      <li></li>
+      <li>Made in C with Pthreads Library</li>
+      <li>Made in C with OMP Library</li>
+      <li>Made in Python with Threading Library</li>
     </ul>
   </li>
   <li>
@@ -17,21 +19,25 @@
     </ul>
   </li>
   <li>
-    <p>MonteCarloPi</p>
+    <p><b>MonteCarloPi:</b> uses MultiThreading to estimate pi using the MonteCarlo algorithm which is a heuristic random algorithm</b></p>
     <ul>
-      <li>hi</li>
+      <li>Made in C with Pthreads Library</li>
+      <li>Made in C++ with OMP Library</li>
     </ul>
   </li>
   <li>
-    <p>PythonPrimeNumbers</p>
+    <p><b>PythonPrimeNumbers:</b> uses MultiThreading to find all prime numbers from 0 to user inputted x.</p>
     <ul>
-      <li>hi</li>
+      <li>Made in Python with Threading Library</li>
     </ul>
   </li>
   <li>
-    <p>ThreadPractice</p>
+    <p><b>Sudoku Solution Checker:</b> uses MultiThreading to check the validity of a 3x3 sudoku solution.</p>
     <ul>
-      <li>hi</li>
+      <li>Made in C with Pthreads Library</li>
+      <li>Made in C++ with OMP Library</li>
+      <li>Made in Python with Threading Library</li>
+      <li>Made in Java</li>
     </ul>
   </li>
 
